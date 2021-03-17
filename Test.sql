0ee98d8484719bbab0a53f41af21433a8aee9a2a
SnehaSomani <72212182+SnehaSomani@users.noreply.github.com>
2021-03-17 19:36:41 +0530

    Add files via upload

---
+++ Test.sql
@@ -0,0 +1,7 @@
+create table student(name varchar(20) not null, Rollno int primary key, Address varchar(20), Mobile_No numeric(10) not null, PAN_No varchar(8));
+
+insert into student values('Kavya', '19111036', 'Pune', '9887695643', 'BCFR4576');
+insert into student values('Reyansh', '19111215', 'Mumbai', '7014567845', 'KHJG8976');
+insert into student values('Aayush', '1911175', 'Nagpur', '9776543876', 'RETG6543');
+insert into student values('Krisha', '1911285', 'Aurangabad', '7056784325', 'SDCVG8907');
+insert into student values('Vedhika', '3245621', 'pune', '9787654790', 'HTIV4560');
